dcbd32b56989fc40097ac1a4f7de285494498128
Sandul Munasinghe <sandultharuna6@gmail.com>
2024-01-24 13:47:20 +0530

    created forms and navigated through form

---
+++ src/com/devstack/edu/Appinitializer.java
@@ -0,0 +1,22 @@
+package com.devstack.edu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("./view/LoginForm.fxml"))));
+        primaryStage.setTitle("EduSmart");
+        primaryStage.show();
+    }
+}
--- src/com/devstack/edu/controller/SignupFormController.java
+++ src/com/devstack/edu/controller/SignupFormController.java
@@ -2,26 +2,24 @@ package com.devstack.edu.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
-public class LoginFormController {
-    public AnchorPane loginFormContext;
+public class SignupFormController {
+    public AnchorPane registerFormContext;
 
-    public void signInOnAction(ActionEvent actionEvent) {
+    public void signUpOnAction(ActionEvent actionEvent) {
     }
 
-    public void createAccountOnAction(ActionEvent actionEvent) throws IOException {
-        setUI("SignupForm");
+    public void alreadyHaveAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("LoginForm");
     }
 
     private void setUI(String location) throws IOException {
-        Stage stage = (Stage) loginFormContext.getScene().getWindow();
+        Stage stage = (Stage) registerFormContext.getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
         stage.setTitle("EduSmart");
         stage.centerOnScreen();
